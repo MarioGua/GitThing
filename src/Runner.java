5e303a39e3e40e12dbba5dd8a2f203c49e9844dd
BKTStudent <sample@bths.edu>
2025-11-21 10:25:12 -0500

    Initial Commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,12 @@
 public class Runner {
     public static void main(String[] args) {
+        int Number1 = 4556;
+        int Number2  = 45;
 
+        if (Number2 * 100 > Number1){
+            System.out.println("num 2 is bigger when multiplied 100 times");
+        } else{
+            System.out.println("num1 is smaller");
+        }
     }
 }
