3100566edda19009e42186065ad9484fa3b0f9ca
Fernando <fernandoguam31@gmail.com>
2025-11-24 20:59:52 -0500

    From home

--- src/ClassTwo.java
+++ src/ClassTwo.java
@@ -1,6 +1,15 @@
 public class ClassTwo {
     public static void main(String[] args) {
+
+        int num100 = 100;
+        double num100do = 100;
         System.out.println("New Class two");
         System.out.println("Hii");
+
+        if (num100 > num100do){
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
     }
 }
