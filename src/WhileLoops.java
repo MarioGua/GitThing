7baed712b532ec165aa20bb399e39d95285d37a3
BKTStudent <sample@bths.edu>
2025-11-25 09:46:12 -0500

    while loops

---
+++ src/WhileLoops.java
@@ -0,0 +1,35 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 5) {
+            System.out.println(counter);
+            counter++;
+        }
+
+//        System.out.println("final counter: " + counter);
+//        int counter = 0;
+//        while (counter < 5) {
+//            counter++;
+//            System.out.println(counter);
+//        }
+//        System.out.println("final counter: " + counter);
+//        int counter = 10;
+//        while (counter < 5) {
+//            System.out.println(counter);
+//            counter++;
+//        }
+//        System.out.println("final counter: " + counter);
+//        int counter = 0;
+//        while (counter < 5) {
+//            System.out.println(counter);
+//        }
+//        System.out.println("final counter: " + counter);
+//        int counter = 0;
+//        while (counter >= 0) {
+//            counter++;
+//        }
+//        System.out.println("final counter: " + counter);
+
+
+    }
+}
